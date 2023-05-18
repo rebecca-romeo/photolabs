a74dcdb7f8f5606634bb33d95c8f10dbee1f58b5
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-05-18 00:01:02 -0400

    display multiple instances of a component using array helper. Add key to each instance

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,10 +4,14 @@ import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 
 // Note: Rendering a single component to build components in isolation
-const App = () => (
-  <div className="App">
-    <PhotoListItem/>
-  </div>
-)
+const App = () => {
 
+  const photos = [<PhotoListItem key="1" />, <PhotoListItem key="2" />, <PhotoListItem  key="3" />]
+
+  return (
+    <div>
+      {photos}
+    </div>
+  )
+}
 export default App
