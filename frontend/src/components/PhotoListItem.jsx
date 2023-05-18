6a3d3b203632c09345e25206dff4a0d1809fa612
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-05-18 00:55:21 -0400

    styling PhotoListItem

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -5,14 +5,25 @@ import '../styles/PhotoListItem.scss';
 
 const PhotoListItem = (props) => {
   return (
-    <article >
-    <img src={props.imageSource} alt="" />
+    <article className='photo-list--item'>
+
+      <img src={props.imageSource} className='photo-list--image' />
+
+      <div className='photo-list--user-details'>
+        <img src={props.imageSource} alt="" className='photo-list--user-profile' />
+        <div className='photo-list--user-info'>
+          <p>{props.username}</p>
+          <p className='photo-list--user-location'>{props.city}</p>
+        </div>
+      </div>
+
     </article>
   )
 }
 
 PhotoListItem.defaultProps = {
   username: 'Jacob',
+  city: 'city, country',
   imageSource: `${process.env.PUBLIC_URL}/Image.jpg`,
   id: 1,
   hideUserName: false,
