52972111361cd6cfd305b9b0c6fa4ffd21586df6
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-05-18 00:58:48 -0400

    destructure props, add short circuit for displaying username

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -4,16 +4,17 @@ import React from 'react';
 import '../styles/PhotoListItem.scss';
 
 const PhotoListItem = (props) => {
+  const { username, location, imageSource, hideUserName } = props;
   return (
     <article className='photo-list--item'>
 
-      <img src={props.imageSource} className='photo-list--image' />
+      <img src={imageSource} className='photo-list--image' />
 
       <div className='photo-list--user-details'>
-        <img src={props.imageSource} alt="" className='photo-list--user-profile' />
+        <img src={imageSource} alt="" className='photo-list--user-profile' />
         <div className='photo-list--user-info'>
-          <p>{props.username}</p>
-          <p className='photo-list--user-location'>{props.city}</p>
+          {!hideUserName && <p>{username}</p>}
+          <p className='photo-list--user-location'>{location}</p>
         </div>
       </div>
 
@@ -23,7 +24,7 @@ const PhotoListItem = (props) => {
 
 PhotoListItem.defaultProps = {
   username: 'Jacob',
-  city: 'city, country',
+  location: 'city, country',
   imageSource: `${process.env.PUBLIC_URL}/Image.jpg`,
   id: 1,
   hideUserName: false,
