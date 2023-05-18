bd124241d3e6d919982eae2c9ea54ed933c31749
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-05-18 15:15:21 -0400

    user can like and unlike a photo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 
-import PhotoListItem from './components/PhotoListItem';
+// import PhotoListItem from './components/PhotoListItem';
+// import FavIcon from './components/FavIcon';
+import PhotoFavButton from './components/PhotoFavButton';
+// import { FavBadge } from './components/FavBadge';
 import './App.scss';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
 
-  const photos = [<PhotoListItem key="1" />, <PhotoListItem key="2" />, <PhotoListItem  key="3" />]
+  // const photos = [<PhotoListItem key="1" />, <PhotoListItem key="2" />, <PhotoListItem  key="3" />]
 
   return (
     <div>
-      {photos}
+      {/* {photos} */}
+      {/* <FavIcon /> */}
+      {/* <FavBadge /> */}
+      <PhotoFavButton />
+
     </div>
   )
 }
--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -1,15 +1,28 @@
 import React, { useCallback, useState } from 'react';
 
 import { FavIcon } from './FavIcon';
+import { FavBadge } from './FavBadge';
 import '../styles/PhotoFavButton.scss';
 
+
+
+// when user clicks the button, say clicked
+// if like is set to false, show heart outline
+// if heart is set to true, show heart filled
+// on click, change the state to opposite (ie true -> false)
+
 function PhotoFavButton() {
+
+  const [like, setLike] = useState(false);
+  const switchLike = () => setLike(like ? false : true);
+  console.log(like)
+
   return (
-    <div className="photo-list--fav-icon">
-      <div className="photo-list--fav-icon-svg">
-        {/* Insert React */}
-      </div>
-    </div>
+    <button
+      className="photo-list--fav-icon"
+      onClick={switchLike}>
+        {like ? <FavBadge /> : <FavIcon  />}
+    </button>
   );
 }
 
