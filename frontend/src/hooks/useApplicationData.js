9a0888ad39ac0e91a567fe977e19a4347912d5c4
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-05-21 13:48:30 -0400

    refactor from useState to useReducer

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,36 +1,57 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
+
+
+
+const reducer = (state, action) => {
+switch (action.type) {
+case 'SET_FAVOURITED_PHOTOS':
+return {
+...state,
+favouritedPhotos: action.payload
+};
+case 'SET_SELECT_PHOTO':
+return {
+...state,
+selectPhoto: action.payload
+};
+case 'SET_MODAL':
+return {
+...state,
+modal: action.payload
+};
+default:
+return state;
+}
+};
 
 const useApplicationData = () => {
+  const initialState = {
+    favouritedPhotos: [],
+    selectPhoto: {},
+    modal: false
+    };
 
-  // State to control which photos are favourited
-  const [favouritedPhotos, setFavouritedPhotos] = useState([]);
+const [state, dispatch] = useReducer(reducer, initialState);
 
-  //State to control which photo is selected in the modal
-  const [selectPhoto, setSelectPhoto] = useState({});
+const openModal = (photo) => {
+dispatch({ type: 'SET_SELECT_PHOTO', payload: photo });
+dispatch({ type: 'SET_MODAL', payload: true });
+};
 
-  // State and fuctions to control whether the modal is open or closed
-  const [modal, setModal] = useState(false);
+const closeModal = () => {
+dispatch({ type: 'SET_SELECT_PHOTO', payload: null });
+dispatch({ type: 'SET_MODAL', payload: false });
+};
 
-
-  const openModal = (photo) => {
-    setSelectPhoto(photo)
-    setModal(true);
-  }
-
-  const closeModal = () => {
-    setSelectPhoto(null)
-    setModal(false);
-  }
-
-  return {
-    modal,
-    openModal,
-    closeModal,
-    selectPhoto,
-    favouritedPhotos,
-    setFavouritedPhotos
-
-  };
+return {
+modal: state.modal,
+openModal,
+closeModal,
+selectPhoto: state.selectPhoto,
+favouritedPhotos: state.favouritedPhotos,
+setFavouritedPhotos: (favouritedPhotos) =>
+dispatch({ type: 'SET_FAVOURITED_PHOTOS', payload: favouritedPhotos })
+};
 };
 
 export default useApplicationData;
